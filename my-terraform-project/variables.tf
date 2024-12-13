e5ea2c0f4d26a21b9b562b7b0214eb348b8602b0
kharratmarie <kharrat.mariam@enis.tn>
2024-12-13 20:46:21 +0100

    terraform modified

--- my-terraform-project/variables.tf
+++ my-terraform-project/variables.tf
@@ -1,51 +1,47 @@
 
-# VPC CIDR Block
 variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16" # Vous pouvez ajuster ce CIDR selon vos besoins
 }
-# Public Subnet 1 CIDR Block
+
 variable "public_subnet_1_cidr" {
-  description = "CIDR block for public subnet 1"
-  type        = string
-  default     = "10.0.1.0/24"
+  description = "CIDR block for Public Subnet 1"
+  default     = "10.0.1.0/24" # CIDR pour le sous-réseau
 }
-# Public Subnet 2 CIDR Block
-variable "public_subnet_2_cidr" {
-  description = "CIDR block for public subnet 2"
-  type        = string
-  default     = "10.0.2.0/24"
-}
-# Availability Zone for Subnet 1
+
 variable "availability_zone_1" {
-  description = "Availability zone for the first public subnet"
-  type        = string
-  default     = "us-east-1a"
+  description = "Availability Zone for Public Subnet 1"
+  default     = "us-east-1a" # Zone de disponibilité souhaitée
 }
-# Availability Zone for Subnet 2
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for Public Subnet 2"
+  default     = "10.0.2.0/24" # CIDR pour le sous-réseau
+}
+
 variable "availability_zone_2" {
-  description = "Availability zone for the second public subnet"
-  type        = string
-  default     = "us-east-1b"
+  description = "Availability Zone for Public Subnet 1"
+  default     = "us-east-1b" # Zone de disponibilité souhaitée
 }
-# SSH Key Name
+
+
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to be created"
-  type        = string
-  default     = "deployer_key"
+description = "The name of the SSH key pair to be created"
+type = string
+default = "deployer_key"
 }
-# EC2 Instance Type
+
+
 variable "instance_type" {
-  description = "The type of EC2 instance"
-  type        = string
-  default     = "t2.micro"
+description = "The type of EC2 instance"
+type = string
+default = "t2.micro"
 }
-# EC2 AMI ID
+
+
 variable "ec2_ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
 #   default     = "ami-0ebfd941bbafe70c6" amazon linux
   default     = "ami-0866a3c8686eaeeba"
 }
-
